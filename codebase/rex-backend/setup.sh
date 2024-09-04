e5e32cb014f145dd2b5056fce9dc38c726e68041
bledsoef <bledsoef@berea.edu>
2024-09-03 21:51:30 -0400

    created setup script to create venv, install from requirements and set pythonpath

---
+++ codebase/rex-backend/setup.sh
@@ -0,0 +1,16 @@
+if [ ! -d venv ]
+then
+  python3 -m venv venv
+fi
+. venv/bin/activate
+
+# upgrade pip
+python3 -m pip install --upgrade pip #added python-m for pip installs (source setup overwrite for venv)
+
+# install requirements
+python3 -m pip install -r requirements.txt
+# To generate a new requirements.txt file, run "pip freeze > requirements.txt"
+
+echo "-----Setting $(pwd) as your PYTHONPATH. If this is not root cd into your root directory and rerun this script."
+current_directory=$(pwd)
+export PYTHONPATH="$current_directory"
