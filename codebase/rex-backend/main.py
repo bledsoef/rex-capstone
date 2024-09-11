ff7c2571098f05c8d099ac0746447e20cb1de270
bledsoef <bledsoef@berea.edu>
2024-09-11 10:28:03 -0400

    connected controllers to main router

--- codebase/rex-backend/main.py
+++ codebase/rex-backend/main.py
@@ -3,6 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware  # Import the CORS middleware
 from mangum import Mangum
 import uvicorn
 from database.config import engine, SessionLocal, Base
+import app.controllers.artists as artists
+import app.controllers.users as users
+import app.controllers.reviews as reviews
+import app.controllers.playlists as playlists
+import app.controllers.rex as rex
+import app.controllers.songs as songs
+import app.controllers.stats as stats
 
 app = FastAPI()
 
@@ -13,6 +20,13 @@ app.add_middleware(
     allow_methods=["*"],  # Allow both POST and OPTIONS methods
     allow_headers=["*"],
 )
+app.include_router(artists)
+app.include_router(reviews)
+app.include_router(rex)
+app.include_router(songs)
+app.include_router(users)
+app.include_router(playlists)
+app.include_router(stats)
 
 Base.metadata.create_all(bind=engine)
 handler = Mangum(app)
