fb74703186f88ad14d567e4835566df67dd84dce
bledsoef <bledsoef@berea.edu>
2024-09-11 14:53:08 -0400

    adding in test data

--- codebase/rex-backend/database/reset_database.py
+++ codebase/rex-backend/database/reset_database.py
@@ -1,12 +1,27 @@
 from database.config import Base, engine
 from sqlalchemy.orm import Session
-from app.models.models import *
+from app.models.User import User
+from app.models.PendingRec import PendingRec
 from datetime import datetime
+from app.models.Rec import Rec
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 with Session(engine) as session:
-    finn = User(username="bledsoef", email="bledsoef@berea.edu", first_name="Finn", last_name="Bledsoe", created_at=datetime(2024, 4, 4), is_artist=False, account_type="paid")
-    session.add_all([finn])
+    bledsoef = User(id=1, username="bledsoef", email="bledsoef@berea.edu", first_name="Finn", last_name="Bledsoe", created_at=datetime(2024, 4, 4), is_artist=False, account_type="paid")
+    bahrs = User(id=2, username="bahrs", email="bahrs@berea.edu", first_name="Summer", last_name="Bahr", created_at=datetime(2024, 4, 4), is_artist=False, account_type="paid")
+    rec_1 = Rec(id=1,
+                title="Test",
+                body="Super sweet song",
+                sender_id=1,
+                recipient_id=2,
+                created_at=datetime(2024, 6, 5),
+                song_id=1,
+                is_post=False)
+    
+    pending_rec_1 = PendingRec(id=1, rec_id=1, updated=datetime(2024, 4, 4))
+    session.add_all([bledsoef, bahrs])
+    session.add_all([rec_1])
+    session.add_all([pending_rec_1])
     session.commit()
