945ad279750d24319cca6ab92c19475e7cc75bbd
bledsoef <bledsoef@berea.edu>
2024-09-18 09:00:56 -0400

    installed firebase auth and added custom tailwind config

--- codebase/rex-react-native/tailwind.config.js
+++ codebase/rex-react-native/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#50BA6F"
+      },
+      fontFamily: {
+      jthin: ["JosefinSansThin", "sans-serif"],
+      jlight: ["JosefinSansLight", "sans-serif"],
+      jregular: ["JosefinSansRegular", "sans-serif"],
+      jsemibold: ["JosefinSansSemibold", "sans-serif"],
+      jbold: ["JosefinSansBold", "sans-serif"],
+    },},
   },
   plugins: [],
 }
