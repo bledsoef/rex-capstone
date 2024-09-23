39f1a755307e858baf7e6d84f33a464a45fec262
bledsoef <bledsoef@berea.edu>
2024-09-22 22:15:23 -0400

    added the rex colors as primary and secondary colors

--- codebase/rex-react-native/constants/Colors.ts
+++ codebase/rex-react-native/constants/Colors.ts
@@ -3,24 +3,24 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#50BA6F';
+const tintColorDark = '#50BA6F';
 
 export const Colors = {
   light: {
-    text: '#11181C',
+    text: '#50BA6F',
     background: '#fff',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#50BA6F',
+    tabIconDefault: '#50BA6F',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
+    text: '#50BA6F',
     background: '#151718',
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: '#50BA6F',
+    tabIconDefault: '#50BA6F',
     tabIconSelected: tintColorDark,
   },
 };
