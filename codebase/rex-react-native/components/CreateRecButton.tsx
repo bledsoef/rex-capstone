bfeb7b01892c3a71735110a533eb2fd2c1e97eb9
bledsoef <bledsoef@berea.edu>
2024-09-27 17:58:16 -0400

    created a button for creating recs

---
+++ codebase/rex-react-native/components/CreateRecButton.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { type ComponentProps } from "react";
+import { Pressable, View, StyleSheet } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+export function CreateRecButton({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}: any) {
+  return (
+    <View className={``}>
+      <Pressable
+        className={`rounded-full flex shadow-xl bg-primary justify-center w-[40] h-[40] items-center p-2`}
+        onPress={handlePress}
+      >
+        <FontAwesome5 color={"white"} size={20} className="" name={"pen"} />
+      </Pressable>
+    </View>
+  );
+}
