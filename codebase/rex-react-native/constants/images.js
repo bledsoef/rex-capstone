cb13b97228056adfa8c4af3263073ea31afdcfb3
bledsoef <bledsoef@berea.edu>
2024-10-02 09:18:37 -0400

    added new assets to codebase

--- codebase/rex-react-native/constants/images.js
+++ codebase/rex-react-native/constants/images.js
@@ -1,3 +1,5 @@
 import logo from "../assets/images/logo.png";
+import profile from "../assets/images/profile.png";
+import default_cover from "../assets/images/default_cover.png";
 
-export default { logo } 
+export default { logo, profile, default_cover } 
