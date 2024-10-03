d209b5dffb3aad015845198c5569c727536f282d
bledsoef <bledsoef@berea.edu>
2024-10-03 00:49:30 -0400

    started on search functionality

--- codebase/rex-react-native/app/(tabs)/search.tsx
+++ codebase/rex-react-native/app/(tabs)/search.tsx
@@ -1,11 +1,30 @@
-import { Image, StyleSheet, Text, SafeAreaView, View } from 'react-native';
-
+import {
+  Image,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  TextInput,
+} from "react-native";
+import { FormField } from "@/components/FormField";
+import { useState } from "react";
 export default function Search() {
+  const [searchQuery, setSearchQuery] = useState<any>("");
+  const [results, setResults] = useState<any[]>([]);
   return (
-   <SafeAreaView className="bg-white min-h-screen">
-    <View>
-      <Text>Hello</Text>
-    </View>
-   </SafeAreaView>
+    <SafeAreaView className="bg-white min-h-screen">
+      <View className={"p-5"}>
+        <FormField
+          title={"Username"}
+          placeholder={"Search"}
+          value={searchQuery}
+          handleChangeText={(e: any) => setSearchQuery(e)}
+          otherStyles={"py-1"}
+        />
+        {results?.map((result, index) => (
+          <Text/>
+        ))}
+      </View>
+    </SafeAreaView>
   );
 }
