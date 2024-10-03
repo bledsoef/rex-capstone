d96d879d99e5ca7e71c47b63318843618f5cc93d
bledsoef <bledsoef@berea.edu>
2024-10-03 00:44:27 -0400

    added album and artist information to global state

--- codebase/rex-react-native/components/PlayBar.tsx
+++ codebase/rex-react-native/components/PlayBar.tsx
@@ -8,35 +8,28 @@ import { useMusicPlayer } from "./PlayerContext";
 import { images } from "@/constants";
 
 export function PlayBar() {
-  const { currentSong, currentAlbum, isPlaying, sound, playSong, togglePlayPause, setSound } = useMusicPlayer();
+  const {
+    currentSong,
+    currentAlbum,
+    currentArtists,
+    isPlaying,
+    sound,
+    playSong,
+    togglePlayPause,
+    setSound,
+  } = useMusicPlayer();
   const [imageUrl, setImageUrl] = useState<string>("");
 
   useEffect(() => {
-    const loadSound = async () => {
-      if (currentSong) {
-        const url = await getAudioDownloadURL();
-        const { sound: newSound } = await Audio.Sound.createAsync({ uri: url });
-        setSound(newSound);
-      }
-    };
-
     if (currentSong) {
-      loadSound();
-      fetchImageDownloadUrl();
+      fetchImageDownloadUrl(); // Keep the image loading logic
     }
-
     return () => {
       if (sound) {
         sound.unloadAsync();
       }
     };
   }, [currentSong]);
-
-  const getAudioDownloadURL = async () => {
-    const fileRef = ref(storage, `/audioFiles/${currentSong.id}.mp3`);
-    const res = await getDownloadURL(fileRef);
-    return res;
-  };
 
   async function fetchImageDownloadUrl() {
     const fileRef = ref(storage, `/albumImages/${currentAlbum.id}.jpg`);
@@ -48,23 +41,41 @@ export function PlayBar() {
   }
 
   return (
-    <View className={`w-[95%] bottom-[9%] rounded-lg justify-center opacity-95 absolute mb-1 right-2 left-2 p-2 bg-[#E8E8E8]`}>
-      <View className="flex flex-row justify-between items-center">
-        <View className="flex flex-row items-center">
-          <Image
-            source={{ uri: imageUrl ? imageUrl : images.default_cover }}
-            className={`w-[60] h-[60] mr-2`}
-            resizeMode="cover"
-          />
-          <View className="flex flex-col">
-            <Text className="flex text-lg font-jsemibold">{currentSong?.name}</Text>
-            <Text className="flex text-base font-jregular">{currentSong?.artist}</Text>
+    <>
+      {currentSong && (
+        <View
+          className={`w-[95%] bottom-[10%] rounded-lg justify-center opacity-95 absolute mb-1 right-2 left-2 p-2 bg-slate-50 border-slate-100 border`}
+        >
+          <View className="flex flex-row justify-between items-center">
+            <View className="flex flex-row items-center">
+              <Image
+                source={{ uri: imageUrl ? imageUrl : images.default_cover }}
+                className={`w-[60] h-[60] mr-2`}
+                resizeMode="cover"
+              />
+              <View className="flex flex-col">
+                <Text className="flex text-lg font-jsemibold">
+                  {currentSong["title"]}
+                </Text>
+                <Text className="flex text-base font-jlight">
+                  {currentArtists &&
+                    currentArtists.map((artist: any, index: number) => {
+                      console.log(artist.name);
+                      if (index != currentArtists.length - 1) {
+                        return `${artist.name}, `;
+                      } else {
+                        return artist.name;
+                      }
+                    })}
+                </Text>
+              </View>
+            </View>
+            <Pressable onPress={togglePlayPause} className="pr-2">
+              <FontAwesome size={25} name={isPlaying ? "pause" : "play"} />
+            </Pressable>
           </View>
         </View>
-        <Pressable onPress={togglePlayPause} className="pr-2">
-          <FontAwesome size={25} name={isPlaying ? "pause" : "play"} />
-        </Pressable>
-      </View>
-    </View>
+      )}
+    </>
   );
 }
--- codebase/rex-react-native/components/PlayerContext.tsx
+++ codebase/rex-react-native/components/PlayerContext.tsx
@@ -1,21 +1,36 @@
 import React, { createContext, useState, useContext } from "react";
+import { ref, getDownloadURL } from "firebase/storage";
+import { storage } from "@/firebaseConfig";
+import { Audio } from "expo-av";
 
 // Create the context
 const MusicPlayerContext = createContext<any>(null);
 
 // Create a provider component
 export const MusicPlayerProvider = ({ children }: any) => {
-  const [currentSong, setCurrentSong] = useState<any>({ name: "Banquet", artist: "Bloc Party", id: 2 });
+  const [currentSong, setCurrentSong] = useState<any>(null);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const [currentAlbum, setCurrentAlbum] = useState<any>({id: 1})
+  const [currentAlbum, setCurrentAlbum] = useState<any>(null)
+  const [currentArtists, setCurrentArtists] = useState<any[]>([])
   const [sound, setSound] = useState<any>(null);
-
-  const playSong = async (song: any) => {
+  const getAudioDownloadURL = async (songId: string) => {
+    const fileRef = ref(storage, `/audioFiles/${songId}.mp3`);
+    const res = await getDownloadURL(fileRef);
+    return res;
+  };
+  const playSong = async (song: any, album: any, artists: any) => {
     if (sound) {
       await sound.unloadAsync();
     }
 
     setCurrentSong(song);
+    setCurrentAlbum(album)
+    setCurrentArtists(artists)
+    const url = await getAudioDownloadURL(song.id);  // Fetch audio URL immediately
+    const { sound: newSound } = await Audio.Sound.createAsync({ uri: url });
+    setSound(newSound);
+  
+    await newSound.playAsync();  // 
     setIsPlaying(true);
   };
 
@@ -35,6 +50,7 @@ export const MusicPlayerProvider = ({ children }: any) => {
       value={{
         currentSong,
         currentAlbum,
+        currentArtists,
         isPlaying,
         sound,
         playSong,
