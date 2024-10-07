a8cf9cbf52bf96a9acfef8ad1e9746e8279f63b0
bledsoef <bledsoef@berea.edu>
2024-10-07 15:02:45 -0400

    created route for searching

--- codebase/rex-backend/app/controllers/media.py
+++ codebase/rex-backend/app/controllers/media.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from database.config import get_db
 from app.logic.songs import *
 from app.logic.albums import *
+from app.logic.utils import *
+
 router = APIRouter()    
 
 @router.post("/playSong")
@@ -43,3 +45,12 @@ async def getSongsForAlbum(album_id: str, db: Session = Depends(get_db)):
     except Exception as e:
         print(e)
         return {"message": f"Failed to fetch songs for album_id {album_id}"} 
+
+@router.get("/search")
+async def Search(query: str, db: Session = Depends(get_db)):
+    try:
+        results = search(db, query)
+        return results
+    except Exception as e:
+        print(e)
+        return {"message": f"Failed to fetch results for query {query}"} 
