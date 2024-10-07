ed61a6bf071a2823d46fec44f925c79354e4e851
bledsoef <bledsoef@berea.edu>
2024-10-07 15:03:34 -0400

    got rid of unused commented code

--- codebase/rex-backend/app/controllers/rex.py
+++ codebase/rex-backend/app/controllers/rex.py
@@ -68,11 +68,6 @@ async def getReceivedRecsForUser(username: str, db: Session = Depends(get_db)):
 async def getFeedForUser(email: str, db: Session = Depends(get_db)):
     try:
         non_user_posts = get_non_user_posts(db, email)
-        # result = []
-        # for post in non_user_posts:
-        #     reviews = [entry.__dict__ for entry in get_post_reviews(db, post.id)]
-        #     result.append({"post":post, "reviews": reviews})
-        # print(result)
         return non_user_posts
     except Exception as e:
         print(e)
