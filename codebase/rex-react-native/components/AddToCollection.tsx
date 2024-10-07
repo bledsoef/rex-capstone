402245fc743ab589707190a867a20ce1f42c8ffa
bledsoef <bledsoef@berea.edu>
2024-10-07 14:54:34 -0400

    created new component for adding a recommendation to your collection

---
+++ codebase/rex-react-native/components/AddToCollection.tsx
@@ -0,0 +1,33 @@
+import { View, Pressable, Text } from "react-native";
+export default function AddToCollection({
+  acceptedStatus,
+  recID,
+  userID,
+}: any) {
+  const addToCollection = async () => {
+    const response = await fetch(`http://127.0.0.1:8000/addToCollection`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ recID: recID, userID: userID }),
+    });
+  };
+  return (
+    <View>
+      {acceptedStatus != true && (
+        <Pressable
+          onPress={addToCollection}
+          className="border rounded-xl border-primary p-2"
+        >
+          <Text className="text-primary font-jbold text-base">Accept Rec</Text>
+        </Pressable>
+      )}
+      {acceptedStatus == true && (
+        <Pressable className="border rounded-xl border-primary  bg-primary p-2">
+          <Text className="text-white font-jbold text-base">Accepted</Text>
+        </Pressable>
+      )}
+    </View>
+  );
+}
