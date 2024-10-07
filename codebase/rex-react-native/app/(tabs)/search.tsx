c519d14b5521745d9da6d8d06f45c58adcd7c1a5
bledsoef <bledsoef@berea.edu>
2024-10-07 15:05:30 -0400

    added searching logic to search page

--- codebase/rex-react-native/app/(tabs)/search.tsx
+++ codebase/rex-react-native/app/(tabs)/search.tsx
@@ -1,29 +1,69 @@
-import {
-  Image,
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  View,
-  TextInput,
-} from "react-native";
-import { FormField } from "@/components/FormField";
+import { Text, SafeAreaView, View } from "react-native";
+import { SearchBar } from "@/components/search/SearchBar";
+import { SongResult } from "@/components/search/SongResult";
 import { useState } from "react";
+import { AlbumResult } from "@/components/search/AlbumResult";
 export default function Search() {
+  const queryDB = async (searchQuery: any) => {
+    const res = await fetch(
+      `http://127.0.0.1:8000/search?query=${searchQuery}`
+    );
+    const data = await res.json();
+    setAlbums(data["albums"]);
+    setArtists(data["artists"]);
+    setSongs(data["songs"]);
+  };
+  const handleSetSearchQuery = (query: any) => {
+    setSearchQuery(query);
+    queryDB(query);
+  };
+
   const [searchQuery, setSearchQuery] = useState<any>("");
-  const [results, setResults] = useState<any[]>([]);
+  const [albums, setAlbums] = useState<any[]>([]);
+  const [artists, setArtists] = useState<any[]>([]);
+  const [songs, setSongs] = useState<any[]>([]);
+
   return (
     <SafeAreaView className="bg-white min-h-screen">
       <View className={"p-5"}>
-        <FormField
-          title={"Username"}
+        <SearchBar
           placeholder={"Search"}
           value={searchQuery}
-          handleChangeText={(e: any) => setSearchQuery(e)}
+          handleChangeText={(e: any) => handleSetSearchQuery(e)}
           otherStyles={"py-1"}
         />
-        {results?.map((result, index) => (
-          <Text/>
-        ))}
+        {songs &&
+          songs.map((song, index) => {
+            return (
+              <SongResult
+                key={index}
+                album={song.album}
+                artists={song.artists}
+                song={song.song}
+              ></SongResult>
+            );
+          })}
+        {/* {artists &&
+          artists.map((artist, index) => {
+            return (
+              <Song
+                key={index}
+                album={artist.album}
+                artist={artist.artist}
+                song={artist.song}
+              ></Song>
+            );
+          })} */}
+        {albums &&
+          albums.map((album, index) => {
+            return (
+              <AlbumResult
+                key={index}
+                album={album.album}
+                artists={album.artists}
+              ></AlbumResult>
+            );
+          })}
       </View>
     </SafeAreaView>
   );
