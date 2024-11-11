441259de72c2c16412e7248c3ace73603a38b4b9
bledsoef <bledsoef@berea.edu>
2024-11-11 10:08:53 -0500

    added new field to db to indicate whether to use the default attachment or not

--- codebase/rex-backend/app/models/Playlist.py
+++ codebase/rex-backend/app/models/Playlist.py
@@ -10,6 +10,7 @@ class Playlist(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     updated_at = Column(Date, nullable=True)
+    has_image = Column(Boolean, nullable=True, default=False)
 
     recs = relationship("Rec", backref="playlist")
     playlist_creators = relationship("PlaylistCreator", backref="playlist") 
